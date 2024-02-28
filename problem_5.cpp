5c40f08b8799f313bc87e43f4f7ab3d71ff3bf8f
MD ZILLUR RAHMAN <zillur.hero40@gmail.com>
2024-02-28 18:37:51 +0600

    tumar nunu

--- problem_5.cpp
+++ problem_5.cpp
@@ -1,1 +1,8 @@
-ki mone korso shob tumare diye dimu. khanki nije kor. tore je pass koraisitasi etai beshi. moga konkhaner. autistic. matha mota. pet mota. lens mota. bichi mota. 
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "ki mone korso shob tumare diye dimu. khanki nije kor. tore je pass koraisitasi etai beshi. moga konkhaner. autistic. matha mota. pet mota. lens mota. bichi mota. ";
+
+    return 0;
+}
